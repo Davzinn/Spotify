ab6f77ab18d4554d1fa4938a88a3946e28ac1abf
davi_hayeck <davzinnxl@gmail.com>
2024-05-16 20:24:23 -0400

    Spotify

---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View className='text-2xl items-center'>
+      <Text>Home</Text>
+    </View>
+  )
+}
